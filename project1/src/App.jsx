993d6d0a3f03387b0f258df5e90f9cb223bb1f3b
ushabh-jain <ushabhjain07@gmail.com>
2025-01-14 20:47:06 +0530

    first commit

---
+++ project1/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const App = () => {
+  return (
+    
+    <div>
+      <div>
+      {/* this is void tag because it is self closing */}
+      <img src="anime1.jpg" alt="" /> 
+      </div>
+      <h1>Hunter X Hunter</h1>
+      <h3>Rating : 4 star</h3>
+      <p>
+        Initial D is a Japanese anime series centered around the underground
+        world of street racing, particularly on winding mountain roads. It
+        follows Takumi Fujiwara, a high school student and a skilled driver who
+        unknowingly hones his abilities delivering tofu in his father Toyota
+        AE86. The series showcases intense racing battles, realistic driving
+        techniques, and character growth. Its iconic eurobeat soundtrack and
+        gripping races have made it a cult classic among car and anime
+        enthusiasts.
+      </p>
+    </div>
+  );
+};
+
+export default App;
